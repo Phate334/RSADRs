b7e8a5d023a5484674e9ba07e1259dd8134ae54d
Phate <phate334@gmail.com>
2015-02-06 02:38:20 +0800

    Characteristic Set define

--- CharacteristicSet.py
+++ CharacteristicSet.py
@@ -2,7 +2,7 @@
 # -------------------------------------------------------------------------------
 # Name:          CharacteristicSet
 # Purpose:       Preparing Data for analysis step by characteristic relation.
-#                Source pre-data is combined as follows FAERS tables:
+# Source pre-data is combined as follows FAERS tables:
 #                DEMO:Patient's basic information contain fields like id,age and gender.
 #                DRUG:Drugs used in each case.
 #                REAC:Symptoms which is produced from DRUG table.
@@ -14,8 +14,23 @@
 # -------------------------------------------------------------------------------
 import pyodbc
 
+
+class CharacteristicSet:
+    def __init__(self, connectInfo):
+        self.conInfo = connectInfo
+        with pyodbc.connect(self.conInfo) as con:
+            with con.cursor() as cursor:
+                rows = cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.Tables")
+                self.tables = [r for r, in rows]
+
+    def test(self):
+        print self.tables
+
+
 def main():
-    print(pyodbc)
+    cs = CharacteristicSet("Trusted_Connection=yes;driver={SQL Server};server=localhost;database=LAN_PREDATA")
+    cs.test()
+
 
 if __name__ == "__main__":
     main()
