685861e2259f6d99723eefbb1b018c28c674ad14
Phate <phate334@gmail.com>
2015-02-06 00:45:52 +0800

    README

---
+++ CharacteristicSet.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+# -------------------------------------------------------------------------------
+# Name:          CharacteristicSet
+# Purpose:       Preparing Data for analysis step by characteristic relation.
+#                Source pre-data is combined as follows FAERS tables:
+#                DEMO:Patient's basic information contain fields like id,age and gender.
+#                DRUG:Drugs used in each case.
+#                REAC:Symptoms which is produced from DRUG table.
+#
+# Author:        Phate
+# Created:        2015/2/5
+# Copyright:    (c) Phate 2015
+# Licence:        <your licence>
+# -------------------------------------------------------------------------------
+import pyodbc
+
+def main():
+    print(pyodbc)
+
+if __name__ == "__main__":
+    main()
