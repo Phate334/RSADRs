17508952c52cc87b288f982357babb1779b52416
Phate <phate334@gmail.com>
2015-02-09 04:13:14 +0800

    Pull data into RAM. Check similarity and tolerance methods.

--- CharacteristicSet.py
+++ CharacteristicSet.py
@@ -16,9 +16,14 @@ import threading
 import Queue
 import pyodbc
 
-connect_infomation = "Trusted_Connection=yes;driver={SQL Server};server=localhost"
+connect_information = "Trusted_Connection=yes;driver={SQL Server};server=localhost"
 source_database = "LAN_PREDATA"
 destination_database = "RSADRs"
+CREATE_TABLE = "CREATE TABLE %s (ISR varchar(20),CASES varchar(max));"
+DROP_TABLE = "DROP TABLE %s"
+AGE_TYPE = ["~5", "18~60", "60~"]
+GENDER_TYPE = ["Male", "Female"]
+LOG_DIR = "D:\\log\\"
 
 
 class CharacteristicThread(threading.Thread):
@@ -26,25 +31,77 @@ class CharacteristicThread(threading.Thread):
     Args:
         queue: already process srouce tables.
     """
-    def __init__(self,queue):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.table = None
+        self.season = None
+
+    def run(self):
+        while True:
+            self.table = self.queue.get(block=True)
+            self.season = self.table[2:]
+            print("start process table %s"%(self.table))
+            self.create_table()
+            # pull this season data into ram.
+            with pyodbc.connect(connect_information, database=source_database) as con:
+                with con.cursor() as cursor:
+                    rows = cursor.execute("SELECT ISR,age,gender,drug,PT FROM %s"%(self.table))
+                    temp = {}
+                    for isr, age, gender, drug, PT in rows:
+                        if age not in AGE_TYPE:
+                            age = None
+                        if gender not in GENDER_TYPE:
+                            gender = None
+                        temp[isr] = (age, gender, drug, PT)
+
+    def similarity(self):
+        """Lost case.
+        """
         pass
 
+    def tolerance(self):
+        """Don't care case.
+        """
+        pass
 
-def similarity():
-    """Lost case.
-    """
-    pass
+    def create_table(self):
+        try:
+            with pyodbc.connect(connect_information, database=destination_database) as con:
+                with con.cursor() as cursor:
+                    cursor.execute(CREATE_TABLE % ("similarity_global_"+self.season))
+                    cursor.execute(CREATE_TABLE % ("similarity_age_"+self.season))
+                    cursor.execute(CREATE_TABLE % ("similarity_gender_"+self.season))
+                    cursor.execute(CREATE_TABLE % ("tolerance_global_"+self.season))
+                    cursor.execute(CREATE_TABLE % ("tolerance_age_"+self.season))
+                    cursor.execute(CREATE_TABLE % ("tolerance_gender_"+self.season))
+                    cursor.commit()
+        except:
+            print("pass")
 
-
-def tolerance():
-    """Don't care case.
-    """
-    pass
+    def drop_table(self):
+        try:
+            with pyodbc.connect(connect_information, database=destination_database) as con:
+                with con.cursor() as cursor:
+                    cursor.execute(DROP_TABLE % ("similarity_global_"+self.season))
+                    cursor.execute(DROP_TABLE % ("similarity_age_"+self.season))
+                    cursor.execute(DROP_TABLE % ("similarity_gender_"+self.season))
+                    cursor.execute(DROP_TABLE % ("tolerance_global_"+self.season))
+                    cursor.execute(DROP_TABLE % ("tolerance_age_"+self.season))
+                    cursor.execute(DROP_TABLE % ("tolerance_gender_"+self.season))
+                    cursor.commit()
+        except:
+            print("pass")
 
 
 def main():
-    pass
-
+    tables_queue = Queue.Queue()  # ready to process data.
+    with pyodbc.connect(connect_information, database=source_database) as con:
+        with con.cursor() as cursor:
+            rows = cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.Tables")
+            for r, in rows:
+                tables_queue.put(r)
+                season = r[2:]
 
 if __name__ == "__main__":
     main()
