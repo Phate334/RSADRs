2785807ab852219ef2b04d5b06d897ea6556f48c
Phate334 <phate334@gmail.com>
2015-03-10 02:16:38 +0800

    new work. search multi case in ever season.

---
+++ CaseSearch.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+# -------------------------------------------------------------------------------
+# Name:          CaseSearch
+# Purpose:       search case by drug or pt in all data.
+#
+# Author:         Phate
+#
+# Created:        2015/3/10
+# Copyright:    (c) Phate 2015
+# Licence:        <your licence>
+# -------------------------------------------------------------------------------
+import pyodbc
+
+connect_information = "driver={SQL Server};server=140.127.218.79;UID=ciluser;PWD=ciluser411"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
