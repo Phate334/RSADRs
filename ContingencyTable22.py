83540538914c3dd6991fb97fa62a4742f3c190ea
Phate334 <phate334@gmail.com>
2015-04-18 12:59:40 +0800

    contingency table .

---
+++ ContingencyTable22.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+# -------------------------------------------------------------------------------
+# Name:         ContingencyTable22
+# Purpose:
+#
+# Author:       Phate
+#
+# Created:      2015/4/18
+# Copyright:    (c) user 2015
+# Licence:      <your licence>
+# -------------------------------------------------------------------------------
+import pyodbc
+
+connect_info = "Trusted_Connection=yes;driver={SQL Server};server=localhost"
+
+
+def get_timeline():
+    pass
+
+
+def find(drugs, symptoms, age=None, gender=None):
+    """output contingency table.
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
